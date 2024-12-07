6ab161a21a8f03ec88b0f9780fee1d1c313c6082
David Liebman <radiodee1@gmail.com>
2024-12-07 18:45:42 -0500

    index.js req.body is object

---
+++ express/src/index.js
@@ -0,0 +1,42 @@
+// index.js
+const express = require('express');
+var cors = require('cors')
+
+const fs = require('fs');
+//const path = require('path');
+const app = express();
+const port = 8008;
+
+app.use(cors())
+app.use(express.text({ type: 'text/plain' }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/users', (req, res) => {
+    res.send('users list!');
+})
+
+app.put('/config/',  function (req, res)  {
+    const filename =   '/home/dave/test.txt';
+    // Create a write stream to the file
+    //const writeStream = fs.createWriteStream(filename);
+    const data =  req.body;
+    res.send('body ' + Object.keys(data) + ' ' + filename);
+    console.log(data)
+    fs.writeFile(filename, data, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error writing file');
+    } else {
+      res.send('Data saved successfully');
+    }
+  });
+  // Handle the incoming data
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
