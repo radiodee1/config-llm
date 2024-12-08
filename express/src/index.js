0ba9ebfc635b87cbff843f4f16e11ce3fd93656e
David Liebman <radiodee1@gmail.com>
2024-12-08 11:18:41 -0500

    middleware

--- express/src/index.js
+++ express/src/index.js
@@ -8,29 +8,32 @@ const app = express();
 const port = 8008;
 
 app.use(cors())
-app.use(express.text({ type: 'text/plain' }));
+//app.use(express.text( {"type":"text/plain"} ));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.get('/users', (req, res) => {
     res.send('users list!');
 })
 
-app.put('/config/',  function (req, res)  {
+app.put('/config/:path',  function (req, res)  {
     const filename =   '/home/dave/test.txt';
+    const fname = req.params.path;
     // Create a write stream to the file
     //const writeStream = fs.createWriteStream(filename);
-    const data =  req.body;
-    res.send('body ' + Object.keys(data) + ' ' + filename);
-    console.log(data)
+    const data = req.body.test; // "content here again...";// req.body;
+
+    //res.send('body ' + Object.keys(data) + ' ' + filename + ' ');
+    console.log(data, fname)
     fs.writeFile(filename, data, (err) => {
     if (err) {
-      console.error(err);
-      res.status(500).send('Error writing file');
+        console.error(err);
+        res.status(500).send('Error writing file');
     } else {
-      res.send('Data saved successfully');
+        res.send('Data saved successfully');
     }
   });
   // Handle the incoming data
