e8fc9e0ea91e19a757b7f8994c1f1a8a6095633d
David Liebman <radiodee1@gmail.com>
2024-12-09 11:15:08 -0500

    rm App.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+//import App from './App.vue'
 import router from './router/index.js'
 import Index from './components/Index.vue'
 
