a45b64dc65b455bde59cb143b3a65f7917f7c8fa
David Liebman <radiodee1@gmail.com>
2025-05-03 08:20:58 -0400

    add user_dir.env to config so far

--- do_00_setup_compose_up.sh
+++ do_00_setup_compose_up.sh
@@ -1,7 +1,21 @@
 #!/bin/bash 
 
+USER_DIR=''
+
+
+if [ $# -ne '1' ]; then
+    echo ""
+    echo "Enter a USER_DIR or leave blank for this user."
+    echo "Enter empty double quotation marks for 'none'."
+else
+    USER_DIR=$1 
+fi 
+
+
+
 cd env 
 
+./setup_user_dir.sh $USER_DIR 
 ./setup_ip.sh 
 
 cd ..
